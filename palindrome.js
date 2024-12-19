66b3b6e8a08323fe9bd32555dd72a12b92596c96
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-12-19 12:57:30 +0800

    IsPalindrome

--- palindrome.js
+++ palindrome.js
@@ -3,12 +3,12 @@ function isPalindrome(){
     let word1 = prompt("Enter the first word (e.g., RACECAR): ");
     let word2 = prompt("Enter the second word (e.g., RECORDER): ");
     
-    console.log("Original String 1:", word1);
-    console.log("Original String 2:", word2);
+    prompt("Original String 1:", word1);
+    prompt("Original String 2:", word2);
     
     const reverseString = (str) => str.split("").reverse().join("");
-    console.log("Reversed String 1:",reverseString(word1));
-    console.log("Reversed String 2:",reverseString(word1));
+    prompt("Reversed String 1:",reverseString(word1));
+    prompt("Reversed String 2:",reverseString(word2));
     
     let isPalindrome1 = word1 === reverseString(word1);
     let isPalindrome2 = word2 === reverseString(word2);
